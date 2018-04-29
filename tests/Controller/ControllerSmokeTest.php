32340c9a6f9b65e37ae60cb66981a9e1a31b7341
Denis Brumann <denis.brumann@sensiolabs.de>
2018-04-29 14:59:05 +0200

    Adds smoke test for homepage.
    ```
    composer require --dev phpunit browser-kit css-selector
    ```

---
+++ tests/Controller/ControllerSmokeTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class ControllerSmokeTest extends WebTestCase
+{
+    /**
+     * @dataProvider providePathsForSmokeTest
+     */
+    public function test_page_is_accessible(string $path)
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $path);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), $crawler->filter('title')->text());
+    }
+
+    public function providePathsForSmokeTest()
+    {
+        yield 'Homepage (route: home)' => ['/'];
+    }
+}
